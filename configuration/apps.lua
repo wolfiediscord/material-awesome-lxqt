74f91a12b9683b6ffe8f7456807224c9029dd0cf
ChrisTitusTech <contact@christitus.com>
2020-07-10 09:57:14 -0500

    fix syntax error in apps

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -15,9 +15,6 @@ return {
     screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
     region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
     delayed_screenshot = 'sleep 10 ; ~/.config/awesome/configuration/utils/screenshot -r',
-    
-    -- Editing these also edits the default program
-    -- associated with each tag/workspace
     browser = 'brave-browser-nightly',
     editor = 'gedit', -- gui text editor
     social = 'discord',
@@ -40,7 +37,7 @@ return {
      'synology-drive -minimized',
      'steam -silent',
      '/usr/bin/barrier',
-     '~/.local/bin/wallpaper' -- wallpaper-reddit script
+     '~/.local/bin/wallpaper', -- wallpaper-reddit script
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
