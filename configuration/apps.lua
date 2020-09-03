9decfaeba1a12ebf832cca90b6be69a9c1de6ad4
wolfiediscord <wolfiediscord@gmail.com>
2020-09-03 08:48:52 -0500

    Initial commit

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -10,28 +10,35 @@ return {
   default = {
     terminal = 'terminator',
     rofi = rofi_command,
-    lock = 'i3lock-fancy',
+    lock = 'xscreensaver-command -lock',
     quake = 'terminator',
     screenshot = 'flameshot full -p ~/Pictures',
     region_screenshot = 'flameshot gui -p ~/Pictures',
     delayed_screenshot = 'flameshot full -p ~/Pictures -d 5000',
-    browser = 'flatpak run org.mozilla.firefox',
-    editor = 'gedit', -- gui text editor
-    social = 'flatpak run org.mozilla.Thunderbird',
+    browser = 'firefox',
+    editor = 'featherpad', -- gui text editor
+    social = 'discord',
     game = rofi_command,
-    files = 'nautilus',
+    files = 'pcmanfm-qt',
     music = rofi_command 
   },
   -- List of apps to start once on start-up
   run_on_start_up = {
-    'picom --config ' .. filesystem.get_configuration_dir() .. '/configuration/picom.conf',
-    'nm-applet --indicator', -- wifi
+    'nm-applet', -- wifi
     'pnmixer', -- shows an audiocontrol applet in systray when installed.
     --'blueberry-tray', -- Bluetooth tray icon
-    'numlockx on', -- enable numlock
-    '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
-    'xfce4-power-manager', -- Power manager
-     'flameshot',
+    -- 'numlockx on', -- enable numlock
+    'lxqt-policykit-agent', -- credential manager
+    'lxqt-powerermanagement', -- Power manager
+    'slimbookbattery', -- tlp assistant
+    'compton', -- transparency
+    'xinput set-prop "appletouch" "libinput Tapping Enabled" 1', --add tap to click functionality
+    'xscreensaver', -- screensaver and locking functionality
+    -- 'flameshot',
+    -- 'synology-drive -minimized',
+    -- 'steam -silent',
+    -- '/usr/bin/barrier',
+    -- '~/.local/bin/wallpaper', -- wallpaper-reddit script
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
