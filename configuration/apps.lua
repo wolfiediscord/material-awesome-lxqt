15e45db7fdde1c1797c2bde1add276004cd591fd
Wolfie <45970663+wolfiediscord@users.noreply.github.com>
2020-09-03 05:53:31 -0500

    Update apps.lua

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -15,7 +15,7 @@ return {
     screenshot = 'flameshot full -p ~/Pictures',
     region_screenshot = 'flameshot gui -p ~/Pictures',
     delayed_screenshot = 'flameshot full -p ~/Pictures -d 5000',
-    browser = 'brave-nightly',
+    browser = 'firefox',
     editor = 'gedit', -- gui text editor
     social = 'discord',
     game = rofi_command,
@@ -32,10 +32,6 @@ return {
     '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     'xfce4-power-manager', -- Power manager
      'flameshot',
-     'synology-drive -minimized',
-     'steam -silent',
-     '/usr/bin/barrier',
-     '~/.local/bin/wallpaper', -- wallpaper-reddit script
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
